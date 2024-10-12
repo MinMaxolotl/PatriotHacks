115814c7983b6618680687ac90eb7bbcbcf07458
Guido Visioni <54970514+GuidoVisioni@users.noreply.github.com>
2024-10-12 01:38:25 -0400

    Delete Data2Spotify.py

--- Data2Spotify.py
+++
@@ -1,9 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="ee0276b06af14ed0b6d37d6b09b82957",
-                                               client_secret="1bb2f35b7b05486cabd9dd4baf85743b",
-                                               redirect_uri="http://localhost:1234",
-                                               scope="user-library-read"))
-
-print(sp.audio_features('https://open.spotify.com/track/1YFoeeHQ7YIWqAqWbJoJLO?si=f9946c5a12e3476f'))
